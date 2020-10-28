e229066fa7262a4079c60ba09a8d03e79deee055
julkon00 <56596043+julkon00@users.noreply.github.com>
2020-10-28 15:15:53 +0100

    Finished and after build

---
+++ KartingGame_JuliaKonarczak/Assets/MusicSurprise.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class MusicSurprise : MonoBehaviour
+{
+    public AudioClip Surprise;
+    public float volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((other.tag =="Player") & (!alreadyPlayed))
+
+        {
+            audio.PlayOneShot(Surprise, volume);
+            alreadyPlayed = true;
+            Debug.Log("Wjechało");
+        }
+    }
+
+}
